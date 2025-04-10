f31ce686f7773ac8858a73d90030b63f7d703f32
Kirill Davidov <ka.davidovdavidov@gmail.com>
2025-04-10 10:37:20 +0500

    New migrations: better vote tracking, no more tags, greater title limits

---
+++ database/migrations/2025_04_09_121939_remove_comment_votes.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn('upvotes');
+            $table->dropColumn('downvotes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->bigInteger('upvotes')->default(0);
+            $table->bigInteger('downvotes')->default(0);
+        });
+    }
+};
